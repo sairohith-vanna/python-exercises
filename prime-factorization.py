6893deb68e6003c457779142cde9d1d8bff1ea7e
Vangala Sai Rohith <vsrohith@VANNA.COM>
2021-10-28 17:38:47 +0530

    PE1 Check prime & prime factors

---
+++ prime-factorization.py
@@ -0,0 +1,19 @@
+def check_is_prime(n):
+    multiples = list(m for m in range(1, n+1) if n%m == 0)
+    return multiples, len(multiples) == 2
+
+def find_prime_factors(multiples: list):
+    i = 1
+    prime_factors = list(pf for pf in multiples if check_is_prime(pf)[1])
+    return prime_factors
+
+def process(n: int):
+    mx = check_is_prime(n)
+    if(mx[1]):
+        print(n, 'is a prime number')
+    else:
+        print(n, 'is a composite number')
+        print('prime factors:', find_prime_factors(mx[0]))
+
+process(31)
+process(2179433)
